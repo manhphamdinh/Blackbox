960aea571073d1b90e847b84fe2f4445b6f7ba85
ChillGuy <veryreal021@gmail.com>
2026-04-13 04:23:46 +0700

    cleaned up some code, improved dynamic layout

--- app/src/main/java/com/example/blackbox/Puzzle3Fragment.java
+++ app/src/main/java/com/example/blackbox/Puzzle3Fragment.java
@@ -36,9 +36,11 @@ public class Puzzle3Fragment extends PuzzleBaseFragment {
     private static final int BOTTOM_RIGHT = 2;  // NO VOLUME
     private static final int BOTTOM_LEFT = 3;   // HEADPHONES
 
+    // CACHE
     private BroadcastReceiver audioReceiver;
     private AudioDeviceCallback audioDeviceCallback;
     private ValueAnimator fluidAnimator;
+    private ImageView fluid;
     private final Runnable stateChecker = new Runnable() {
         @Override
         public void run() {
@@ -63,11 +65,12 @@ public class Puzzle3Fragment extends PuzzleBaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.activity_puzzle3, container, false);
 
-        // Initialize boxes
+        // CACHE
         for (int i = 0; i < boxes.length; i++) {
             boxes[i] = root.findViewById(boxIds[i]);
         }
 
+        fluid = root.findViewById(R.id.fluid);
         return root;
     }
 
@@ -79,6 +82,7 @@ public class Puzzle3Fragment extends PuzzleBaseFragment {
         for (int index : getCompletedThisRun()) {
             applyCurrentProgress(boxes[index]);
         }
+
     }
 
     @Override
@@ -188,8 +192,6 @@ public class Puzzle3Fragment extends PuzzleBaseFragment {
             updatePuzzle(boxes[BOTTOM_LEFT], BOTTOM_LEFT);
         }
 
-
-
         // ---- Fluid UI ----
         updateFluid(volume, max);
     }
@@ -199,19 +201,17 @@ public class Puzzle3Fragment extends PuzzleBaseFragment {
         View root = getView();
         if (root == null) return;
 
-        ImageView fluid = root.findViewById(R.id.fluid);
-
         int deviceHeight = MainActivity
                 .getDeviceHeightAndWidth(requireContext()).first;
 
         float ratio = max > 0 ? (float) volume / max : 0;
-        int targetHeight = (int) (deviceHeight * ratio);
+        int currentHeight = (int) (deviceHeight * ratio);
 
         ViewGroup.LayoutParams params = fluid.getLayoutParams();
-        if (lastHeight == targetHeight) {
+        if (lastHeight == currentHeight) {
             return;
         }
-        lastHeight = targetHeight;
+        lastHeight = currentHeight;
 
         int startHeight = params.height;
 
@@ -219,7 +219,7 @@ public class Puzzle3Fragment extends PuzzleBaseFragment {
             fluidAnimator.cancel();
         }
 
-        fluidAnimator = ValueAnimator.ofInt(startHeight, targetHeight);
+        fluidAnimator = ValueAnimator.ofInt(startHeight, currentHeight);
         fluidAnimator.setDuration(300);
         fluidAnimator.setInterpolator(new DecelerateInterpolator());
 
